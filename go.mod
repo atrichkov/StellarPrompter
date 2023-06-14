97f0083d209ed1ce88bd14e4bd4bcd3804786775
Asen <asen.trichkov1@gmail.com>
2023-06-15 01:21:16 +0300

    feat: add payment option

--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.20
 require github.com/stellar/go v0.0.0-20230606222445-da99d595db9a
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi v4.1.2+incompatible // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/manucorporat/sse v0.0.0-20160126180136-ee05b128a739 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/segmentio/go-loggly v0.5.1-0.20171222203950-eb91657e62b2 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stellar/go-xdr v0.0.0-20211103144802-8017fc4bdfee // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/txnbuild"
 )
 
 func main() {
 
 	initMsg := `Hello from stellar bot choose from options below: \n
   1. Generate keypair
-  2. Create account`
+  2. Create account
+  3. Send payment`
 
 	fmt.Println(initMsg)
 
@@ -44,6 +48,74 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println(string(body))
+	} else if selectedOption == "3" {
+		var destination string
+		fmt.Print(`Provide your destination's address: `)
+		fmt.Scanln(&destination)
+
+		var amount string
+		fmt.Print(`Provide your amount (in XLM): `)
+		fmt.Scanln(&amount)
+
+		var secret string
+		fmt.Print(`Provide your secret: `)
+		fmt.Scanln(&secret)
+
+		client := horizonclient.DefaultTestNetClient
+
+		// Make sure destination account exists
+		destAccountRequest := horizonclient.AccountRequest{AccountID: destination}
+		destinationAccount, err := client.AccountDetail(destAccountRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Destination Account", destinationAccount)
+
+		// Load the source account
+		sourceKP := keypair.MustParseFull(secret)
+		sourceAccountRequest := horizonclient.AccountRequest{AccountID: sourceKP.Address()}
+		sourceAccount, err := client.AccountDetail(sourceAccountRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		// Build transaction
+		tx, err := txnbuild.NewTransaction(
+			txnbuild.TransactionParams{
+				SourceAccount:        &sourceAccount,
+				IncrementSequenceNum: true,
+				BaseFee:              txnbuild.MinBaseFee,
+				Preconditions: txnbuild.Preconditions{
+					TimeBounds: txnbuild.NewInfiniteTimeout(),
+				},
+				Operations: []txnbuild.Operation{
+					&txnbuild.Payment{
+						Destination: destination,
+						Amount:      amount,
+						Asset:       txnbuild.NativeAsset{},
+					},
+				},
+			},
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		tx, err = tx.Sign(network.TestNetworkPassphrase, sourceKP)
+		if err != nil {
+			panic(err)
+		}
+
+		resp, err := horizonclient.DefaultTestNetClient.SubmitTransaction(tx)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Successful Transaction:")
+		fmt.Println("Ledger:", resp.Ledger)
+		fmt.Println("Hash:", resp.Hash)
 	}
 
 }
